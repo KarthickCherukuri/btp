e527feaaeffbbc1553842e411d7188a56264aa7b
karthick <karthickcherukuri@gmail.com>
2025-01-16 23:41:06 +0530

    add: connection log

--- dist/SensorListner.js
+++ dist/SensorListner.js
@@ -65,7 +65,7 @@ class SensorListner {
                 }
             });
         };
-        this.socket = new SocketHandler_1.default(serverRole === "slave" ? "http://172.20.10.2:3000" : undefined);
+        this.socket = new SocketHandler_1.default(serverRole === "slave" ? "http://192.168.40.221:3000" : undefined);
         this.serverRole = serverRole;
         this.socket.attachEventListner("test-response", (data) => {
             console.log("test response", data);
